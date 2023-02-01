e79e0be45c34505c7089381ada88204f87770c47
vakacharitha <110221085+vakacharitha@users.noreply.github.com>
2023-02-01 09:20:22 +0530

    first commit

--- src/components/Meals/MealsSummary.jsx
+++ src/components/Meals/MealsSummary.jsx
@@ -3,14 +3,14 @@ import classes from "./MealsSummary.module.css";
 const MealsSummary = () => {
   return (
     <section className={classes.summary}>
+      <h1>Food you love,
+      Food we serve</h1>
       <h2>Delicious Food, Delivered To You</h2>
       <p>
-        Choose your favorite meal from our broad selection of available meals
-        and enjoy a delicious lunch or dinner at home.
+        Choose your favorite meal enjoy a delicious lunch or dinner at home.
       </p>
       <p>
-        All our meals are cooked with high-quality ingredients, just-in-time and
-        of course by experienced chefs!
+      Order food from favourite restaurants near you.
       </p>
     </section>
   );
